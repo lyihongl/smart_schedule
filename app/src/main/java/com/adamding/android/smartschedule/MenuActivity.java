05f422d60870e69fad03bdee165c90d2d619bbd8
Yihong Liu <yihongliu00@gmail.com>
2018-09-15 13:23:59 -0400

    stash

--- app/src/main/java/com/adamding/android/smartschedule/MenuActivity.java
+++ app/src/main/java/com/adamding/android/smartschedule/MenuActivity.java
@@ -2,6 +2,10 @@ package com.adamding.android.smartschedule;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MenuActivity extends AppCompatActivity {
 
@@ -9,5 +13,19 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+        final String date[] = new String[1];
+        Button submit = findViewById(R.id.submit);
+        final EditText dateField = findViewById(R.id.edit);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                date[0] = dateField.getText().toString();
+                System.out.println(date[0]);
+            }
+
+        });
+
+
+
     }
 }
