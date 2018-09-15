eca2ce5805b744f880f7f25246f934bf5da75672
wqding <42283702+wqding@users.noreply.github.com>
2018-09-15 18:32:24 -0400

    added new activity

--- app/src/main/java/com/adamding/android/smartschedule/MenuActivity.java
+++ app/src/main/java/com/adamding/android/smartschedule/MenuActivity.java
@@ -1,13 +1,60 @@
 package com.adamding.android.smartschedule;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
+import java.util.ArrayList;
 
 public class MenuActivity extends AppCompatActivity {
+
+    //ArrayList<String> input = new ArrayList<String>();
+    EditText eventIn, dateIn, timeIn;
+
+    Button subButton, backButton;
+
+    //TextView subText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        //initializing variables
+        subButton =(Button) findViewById(R.id.subButton);
+        //subText = (TextView) findViewById(R.id.subText);
+        eventIn = (EditText) findViewById(R.id.editTextEvent);
+        dateIn = (EditText) findViewById(R.id.editTextDate);
+        timeIn = (EditText) findViewById(R.id.editTextTime);
+
+        //
+        subButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+               // subText.setText("Submitted! Go on with your day");
+
+                //storing user input into variables
+                String event = eventIn.getText().toString();
+                int date = Integer.valueOf(dateIn.getText().toString());
+                int time = Integer.valueOf(timeIn.getText().toString());
+            }
+        });
+
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
+
+//    public void switchToev(View view){
+//        Intent startEventAct = new Intent (this, MainActivity.class);
+//        startActivity(startEventAct);
+//    }
 }
